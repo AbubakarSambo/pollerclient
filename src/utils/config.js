2a82c83e973b79ece81c3cad4dacc3f15f220d98
Abubakar Sambo <abubakar.sambo@flexisaf.com>
2018-12-24 11:35:01 +0100

    added postbuild and changed endpoint

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,10 +3,7 @@ let BASE_URL;
 if (process.env.NODE_ENV === "development") {
   BASE_URL = "http://localhost:3000";
 } else {
-  // get production client domain from the browser location
-  // eg ics.mykid.school => ['ics', 'mykid', 'school']
-  const clientName = window.location.hostname.split(".")[0];
-  BASE_URL = `https://${clientName}.safsms.cloud/`;
+  BASE_URL = "https://pollzapp.herokuapp.com";
 }
 
 module.exports = {
